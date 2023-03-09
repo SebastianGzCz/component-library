863cc589a7b8434652800b1b375f7d22658e92c0
sebastian <sebastian.gc@globant.com>
2023-03-09 11:37:56 -0500

    prueba commit

--- scripts/build-custom-properties.ts
+++ scripts/build-custom-properties.ts
@@ -1,37 +1,64 @@
 const { writeFile } = require('fs');
-// const { tokens } = require('../tokens.ts');
 const file = require('../tokens.json');
 
 function transformTokens(parentKey, object) {
-    let tokensTransformed = '';
+  let tokensTransformed = '';
 
-    for (const [key, value] of Object.entries(object)) {
-        if (typeof value === 'object') {
-            const customProperty = parentKey ? `${parentKey}-${key}` : key;
-            tokensTransformed += `\n${transformTokens(customProperty, value)}`;
-        } else if (key === 'value') {
-            const variableName = `$${parentKey}`;
-            tokensTransformed += `${variableName}: ${value};`;
-        }
+  for (const [key, value] of Object.entries(object)) {
+    if (typeof value === 'object') {
+      const customProperty = parentKey ? `${parentKey}-${key}` : key;
+      tokensTransformed += `\n${transformTokens(customProperty, value)}`;
+    } else if (key === 'value') {
+      const variableName = `$${parentKey}`;
+      tokensTransformed += `${variableName}: ${value};`;
     }
+  }
 
-    return tokensTransformed;
+  return tokensTransformed;
 }
 
 function buildCustomProperties() {
-    const tokens = file['global'];
-    const tokenNames = Object.keys(tokens);
+  const tokens = file['global'];
+  const tokenNames = Object.keys(tokens);
 
-    tokenNames.forEach((tokenName) => {
-        const tokenValue = tokens[tokenName];
-        const transformedTokens = transformTokens(null, tokenValue).trim();
-        
-        const filename = `/home/runner/work/component-library/component-library/tokens/${tokenName}.scss`;
-        writeFile(filename, transformedTokens, 'utf8', (error) => {
-            if (error) throw error;
-            console.log(`The file ${filename} has been created!`);
-        });
+  tokenNames.forEach((tokenName) => {
+    const tokenValue = tokens[tokenName];
+    const transformedTokens = transformTokens(null, tokenValue).trim();
+    const filename = `./tokens/${tokenName}.scss`;
+
+    writeFile(filename, transformedTokens, 'utf8', (error) => {
+      if (error) throw error;
+      console.log(`The file ${filename} has been created!`);
+
+      // Agrega los archivos creados al repositorio
+      const { exec } = require('child_process');
+      exec('git add tokens/*', (error, stdout, stderr) => {
+        if (error) {
+          console.log(`Error al agregar los archivos: ${error}`);
+          return;
+        }
+        console.log(`Archivos agregados: ${stdout}`);
+      });
+
+      // Confirma los cambios en el repositorio
+      exec('git commit -m "Agrega archivos generados por build-custom-properties"', (error, stdout, stderr) => {
+        if (error) {
+          console.log(`Error al confirmar los cambios: ${error}`);
+          return;
+        }
+        console.log(`Cambios confirmados: ${stdout}`);
+      });
+
+      // Empuja los cambios al repositorio
+      exec('git push', (error, stdout, stderr) => {
+        if (error) {
+          console.log(`Error al empujar los cambios: ${error}`);
+          return;
+        }
+        console.log(`Cambios empujados: ${stdout}`);
+      });
     });
+  });
 }
 
 buildCustomProperties();
